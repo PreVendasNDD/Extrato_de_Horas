dea352e65d985eea735155500bca572ef483648b
PreVendasNDD <joao.pinheiro@ndd.tech>
2026-08-25 11:28:22 -0300

    feat: adiciona configuracao central da API no frontend

---
+++ auth/config.js
@@ -0,0 +1,6 @@
+window.NDD_PORTAL_CONFIG = Object.freeze({
+  // Trocar pela URL HTTPS real quando a API for publicada.
+  apiBaseUrl: 'https://SUA-API-AQUI',
+  loginPage: '/Extrato_de_Horas/login.html',
+  portalPage: '/Extrato_de_Horas/index.html'
+});
